8f2d50baf985cb79420e8421cd6920b7471b358a
💻 Eher <alexandre@eher.com.br>
2025-02-15 03:19:55 +0100

    Upgrade Ubuntu and enable monitoring

--- main.tf
+++ main.tf
@@ -19,20 +19,16 @@ resource "digitalocean_ssh_key" "default" {
 }
 
 resource "digitalocean_droplet" "docker_droplet" {
-  image     = "ubuntu-20-04-x64"
-  name      = "docker-droplet"
-  region    = var.region
-  size      = "s-1vcpu-1gb"
-  tags      = ["docker-instance"]
-  ssh_keys  = [digitalocean_ssh_key.default.id]
-  user_data = <<-EOF
+  image      = "ubuntu-24-10-x64"
+  name       = "docker-droplet"
+  region     = var.region
+  size       = "s-1vcpu-1gb"
+  monitoring = true
+  tags       = ["docker-instance"]
+  ssh_keys   = [digitalocean_ssh_key.default.id]
+  user_data  = <<-EOF
     #!/bin/bash
-    apt-get update
-    apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    apt-get update
-    apt-get install -y docker-ce
-    usermod -aG docker \$(whoami)
+    apt update
+    apt install --yes docker.io
     EOF
 }
