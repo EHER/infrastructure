57a04cb3948c9f254d5a249a61acac72ec82e12c
💻 Eher <alexandre@eher.com.br>
2025-02-19 21:25:52 +0100

    Add firewall to protect ssh and proxy admin access

--- main.tf
+++ main.tf
@@ -13,6 +13,10 @@ provider "digitalocean" {
   token = var.do_token
 }
 
+data "http" "my_ip" {
+  url = "http://ipv4.icanhazip.com"
+}
+
 resource "digitalocean_ssh_key" "default" {
   name       = "default"
   public_key = file("~/.ssh/id_rsa.pub")
@@ -34,4 +38,63 @@ resource "digitalocean_droplet" "docker_droplet" {
     curl -o .env https://raw.githubusercontent.com/EHER/deployment/main/.env.dist
     docker-compose up -d
     EOF
+}
+
+resource "digitalocean_firewall" "default" {
+  name    = "docker-droplet-firewall"
+  droplet_ids = [digitalocean_droplet.docker_droplet.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = [chomp(data.http.my_ip.response_body)]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "25"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "81"
+    source_addresses = [chomp(data.http.my_ip.response_body)]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "465"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "587"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  outbound_rule {
+    protocol = "tcp"
+    port_range = "1-65535"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+
+  outbound_rule {
+    protocol = "udp"
+    port_range = "1-65535"
+    destination_addresses = ["0.0.0.0/0"]
+  }
 }
